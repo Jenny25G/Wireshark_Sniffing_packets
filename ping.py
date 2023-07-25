3a361917a02ecbb6227e92c7d0364692d79b0c90
Shirlyn Jenita J <87718889+Jenny25G@users.noreply.github.com>
2023-07-25 10:53:31 +0530

    Add files via upload

---
+++ ping.py
@@ -0,0 +1,13 @@
+└─# ping testfire.net
+PING testfire.net (65.61.137.117) 56(84) bytes of data.
+^C
+--- testfire.net ping statistics ---
+12 packets transmitted, 0 received, 100% packet loss, time 11277ms
+
+                                                                                                                   
+┌──(root㉿kali)-[/home/kali]
+└─# ping testfire.net
+PING testfire.net (65.61.137.117) 56(84) bytes of data.
+^C
+--- testfire.net ping statistics ---
+114 packets transmitted, 0 received, 100% packet loss, time 115736ms
